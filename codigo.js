a1c2b88fe923d5bf5bb1b04f675241fcbd008a52
Cande Peña <136141678+canduupe@users.noreply.github.com>
2024-09-22 23:31:09 -0300

    ..

---
+++ codigo.js
@@ -0,0 +1,5 @@
+function envio(){
+    var nombre = document.getElementById("txtNombre").value;
+    var mensaje = document.getElementById("txtMensaje").value;
+    alert("Te damos la bienvenida " + nombre + ", su mensaje fue enviado"); 
+}
